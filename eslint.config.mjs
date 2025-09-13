4ec634b182de0d7df1ba234f54fa6e31a6372fb2
Anthony Byledbal <anthony.byledbal@icloud.com>
2025-09-13 05:55:31 +0100

    [NZT-43] Update tsconfig (#86)
    * [NZT-43] Update tsconfig
    
    * [NZT-43] Update tsconfig
    
    * [NZT-43] Update tsconfig
    
    * [NZT-43] Update tsconfig
    
    * [NZT-43] Update tsconfig

---
+++ eslint.config.mjs
@@ -0,0 +1,86 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import _import from "eslint-plugin-import";
+import globals from "globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/dist/**",
+      "**/out/**",
+      "**/public/**",
+      "**/playwright-report/**",
+      "**/coverage/**",
+      "**/coverage/lcov-report/**",
+      "**/*.min.js",
+      "**/*.bundle.js",
+    ],
+  },
+
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        "next/core-web-vitals",
+        "prettier",
+        "eslint:recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings",
+        "plugin:react-hooks/recommended",
+      ),
+    ),
+
+    plugins: {
+      import: fixupPluginRules(_import),
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["unknown"]],
+
+          pathGroups: [
+            {
+              pattern: "@testing-library/react",
+              group: "unknown",
+              position: "before",
+            },
+            {
+              pattern: "STYLES",
+              group: "unknown",
+              position: "after",
+            },
+          ],
+
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+]);
