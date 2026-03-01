fe5ec19f8a0912e927ac413dd5272ea05de456af
ByAnthony <anthony.byledbal@icloud.com>
2026-03-01 21:41:17 +0100

    resources component

--- components/HomePage/Resources/Resources.tsx
+++ components/HomePage/Resources/Resources.tsx
@@ -1,3 +1,9 @@
+import STYLES from "./Resources.module.scss";
+
 export function Resources() {
-  return <h2>Resources to Explore</h2>;
+  return (
+    <div id="resources" className={STYLES["resources"]}>
+      <h2>Resources to Explore</h2>
+    </div>
+  );
 }
