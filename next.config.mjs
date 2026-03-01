676a1b26709dae6d1c1afc9aff70cdb667798d92
Anthony Byledbal <anthony.byledbal@icloud.com>
2026-03-01 21:24:08 +0000

    [NZT-62] no babel (#172)

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    "react-markdown",
-    "remark-gfm",
-    "remark-remove-comments",
-    "rehype-raw",
-    "unist-util-visit",
-    "devlop",
-    "hast-util-to-jsx-runtime",
-    "html-url-attributes",
-    "mdast-util-to-hast",
-    "remark-parse",
-    "remark-rehype",
-    "unified",
-    "vfile",
-  ],
   trailingSlash: true,
   // prod testing on /staging
   // basePath: "/staging",
