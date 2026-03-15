8485880c59d8436d3ddf7637b39288a37578aef1
Anthony Byledbal <anthony.byledbal@icloud.com>
2026-03-15 09:48:31 +0000

    [NZT-65] Update queries for locale (#185)
    * [NZT-65] Update queries for locale
    
    * as

--- utils/database/queries/companyEventsQuery.ts
+++ utils/database/queries/companyEventsQuery.ts
@@ -5,10 +5,10 @@ import { SingleEventData } from "@/types/tunneller";
 export const companyEventsQuery = async (connection: PoolConnection) => {
   const query = `SELECT
     DATE_FORMAT(company_events.company_events_date, '%Y-%m-%d') AS date
-    , company_events.company_events_event AS event
-    , company_events.company_events_title AS title
+    , company_events.company_events_event_en AS event
+    , company_events.company_events_title_en AS title
     , company_events.company_events_img AS image
-    , company_events.company_events_img_alt AS imageAlt
+    , company_events.company_events_img_alt_en AS imageAlt
     FROM company_events
     ORDER BY date ASC`;
 
