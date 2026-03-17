0a761dd2fcd999ec118a6552a84857577058217a
Anthony Byledbal <anthony.byledbal@skyscanner.net>
2026-03-17 09:09:34 +0100

    img sources

--- __tests__/unit/utils/mocks/mockTunneller.ts
+++ __tests__/unit/utils/mocks/mockTunneller.ts
@@ -236,7 +236,7 @@ export const mockImageArchives: ImageArchives = {
   reference: "MS-93/157",
 };
 
-export const mockImageFamily: string = "Courtesy of John Doe family";
+export const mockImageFamily: string = "John Doe";
 
 export const mockImageNewspaper: ImageNewspaper = {
   name: "Auckland Star",
