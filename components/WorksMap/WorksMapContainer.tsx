59f6ab925a8dec371c0884c6299700fc45114526
ByAnthony <anthony.byledbal@icloud.com>
2026-03-20 05:29:35 +0100

    path next component

--- components/WorksMap/WorksMapContainer.tsx
+++ components/WorksMap/WorksMapContainer.tsx
@@ -6,7 +6,10 @@ import { WorkData } from "@/utils/database/queries/worksQuery";
 
 const WorksMap = dynamic(
   () => import("@/components/WorksMap/WorksMap").then((m) => m.WorksMap),
-  { ssr: false, loading: () => <div style={{ height: "600px" }} /> },
+  {
+    ssr: false,
+    loading: () => <div style={{ height: "600px" }} />,
+  },
 );
 
 type Props = {
